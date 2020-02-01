6c0ee3cf719b7423858cef2e0ab23f7cca499653
billsioros <billsioros97@gmail.com>
2020-02-01 18:32:27 +0200

    replaced .config with .mpisrc + uninstall

--- setup.sh
+++ setup.sh
@@ -3,15 +3,40 @@
 . MPIs/mpis-ui
 
 MPIS_DIR="MPIs"
-BASHRC_PATH=".bashrc"
 MPISRC_PATH=".mpisrc"
 
-mpirc_contents=\
-"""
-export MPIS_DIR=~/bin/${MPIS_DIR}
-export PATH=~/bin/$MPIS_DIR:\$PATH
+if [[ "$*" == *--uninstall* ]]
+then
+    test -f ~/"${MPISRC_PATH}" && rm -vr ~/"${MPISRC_PATH}"
+    test -d ~/bin/"${MPIS_DIR}" && rm -vr ~/bin/"${MPIS_DIR}"
 
-. ~/bin/${MPIS_DIR}/.mpis.config
+    log "MESSAGE" "Consider removing any MPIs related entries from ~/.bashrc"
+
+    exit 0
+fi
+
+mpirc_contents=\
+"""#!/bin/bash
+
+MPIS_DIR=~/bin/${MPIS_DIR}
+
+export MPIS_COMPILER=\"\${MPIS_DIR}/mpis-compile\"
+
+export MPIS_SCHEDULER=\"\${MPIS_DIR}/mpis-schedule\"
+
+export MPIS_UI=\"\${MPIS_DIR}/mpis-ui\"
+
+export MPIS_OUTPUT_ROOT=\"out\"
+
+export MPIS_STDOUT_EXTENSION=\"stdout.log\"
+export MPIS_STDERR_EXTENSION=\"stderr.log\"
+
+export MPIS_USER_ID="$( whoami )"
+
+export MPIS_EDITOR=\"code\"
+export MPIS_EDITOR_ARGS=\"-wr\"
+
+export PATH=\"\$MPIS_DIR\":\$PATH
 
 module load mpiP
 module load openmpi3
@@ -19,28 +44,28 @@ module load openmpi3
 
 bashrc_contents=\
 """
-# setting up MPIs
+# MPIs
 if [ -f ~/${MPISRC_PATH} ]; then
     . ~/${MPISRC_PATH}
 fi
-# setting up MPIs
+# MPIs
 """
 
-log "MESSAGE" "Creating '~/${MPISRC_PATH}'"
+MPISRC_PATH=~/"${MPISRC_PATH}"
 
-if [[ ! -f ~/"$MPISRC_PATH" ]] || confirm "WARNING" "Would you like to overwrite ${MPIRC_PATH}"
+log "MESSAGE" "Creating '${MPISRC_PATH}'"
+
+if [[ ! -f "$MPISRC_PATH" ]] || confirm "WARNING" "Would you like to overwrite ${MPISRC_PATH}"
 then
-    echo -n "$mpirc_contents" > ~/"$MPISRC_PATH"
+    echo -n "$mpirc_contents" > "$MPISRC_PATH"
 fi
 
-log "MESSAGE" "Adding MPIs to '~/${BASHRC_PATH}'"
+log "MESSAGE" "Adding MPIs to '~/.bashrc'"
 
-echo -n "$bashrc_contents" >> ~/"$BASHRC_PATH"
+echo -n "$bashrc_contents" >> ~/.bashrc
 
 log "MESSAGE" "Installing MPIs in '~/bin/${MPIS_DIR}'"
 
 mkdir -p ~/bin/"${MPIS_DIR}"; cp -r ./"${MPIS_DIR}" ~/bin/
 
-log "MESSAGE" "Done"
-
-log "MESSAGE" "Consider re-opening the terminal"
+log "MESSAGE" "Restart the terminal"
